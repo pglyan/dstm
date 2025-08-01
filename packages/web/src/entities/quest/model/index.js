fb2a666195ebd6aa3cef778a53f8f28cf32a8a62
Pavel Glyan <>
2025-08-01 14:53:36 +0300

    feature(web): add quest model

--- packages/web/src/entities/quest/model/index.js
+++ packages/web/src/entities/quest/model/index.js
@@ -1,5 +1,15 @@
 import { computed, signal } from '@preact/signals'
 
+export class Quest {
+  constructor(data) {
+    this._id = data.id
+    this.name = signal(data.name)
+    this.status = signal(data.status)
+  }
+
+  get id() { return this._id }
+}
+
 export const quests = signal([])
 
 export function addNew(quest) {
